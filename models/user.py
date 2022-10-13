60e2ce01b96f5550437d67853f07713b5ce52ca7
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-10-13 03:19:28 -0700

    added Place class DB mapping

--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -14,3 +16,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+
+    places = relationship("Place", backref="user", cascade="delete")
