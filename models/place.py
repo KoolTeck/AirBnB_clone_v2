4450b5cf9e1939a011269ce433a7c8188c8ab3dc
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-10-13 03:38:42 -0700

    updated

--- models/place.py
+++ models/place.py
@@ -22,24 +22,20 @@ class Place(BaseModel, Base):
     description = Column(String(1024))
     number_rooms = Column(
         Integer,
-        nullable=False,
         default=0
     )
     number_bathrooms = Column(
         Integer,
-        nullable=False,
         default=0
     )
     max_guest = Column(
         Integer,
-        nullable=False,
         default=0
     )
     price_by_night = Column(
         Integer,
-        nullable=False,
         default=0
     )
-    latitude = Column(Float, default=0)
-    longitude = Column(Float, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
