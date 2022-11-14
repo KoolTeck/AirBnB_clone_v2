13eff480b2758c7036d2158cbdc3c028f3223208
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-11-14 05:19:46 -0800

    added script skipping a variadic routing in flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" The 3-python_route.py module: defines flask usage """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ displays hello at the root endpoint """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ returns HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    "returns C with the value of the text variable"
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """ returns Python with the value of the var """
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
