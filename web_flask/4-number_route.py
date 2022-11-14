6bd45a53844ef1aed3e9852a9da900d387b11b30
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-11-14 05:31:07 -0800

    added flask script contaning url variable with int type

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" The 4-number_route.py module: defines flask usage """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ displays hello at the root endpoint """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ returns HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    "returns C with the value of the text variable"
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """ returns Python with the value of the var """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num_route(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
