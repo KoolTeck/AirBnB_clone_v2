9a8cbd7d125264198c1201de777a65f97118008c
Adetunji Joel <bowale.adetunji@yahoo.com>
2022-11-18 00:56:34 -0800

    updated task 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+""" the 8-cities_by_states.py module """
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exc):
+    """ release sqlalchemy session """
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def c_by_s_route():
+    """ displays cities linked with states """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
